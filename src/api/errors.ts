d0181d896e7da6181f279d7b1fbcfe7fb131103c
Joanne Lin <y3933y3933@gmail.com>
2025-06-04 01:05:14 +0800

    feat: add custom error

---
+++ src/api/errors.ts
@@ -0,0 +1,24 @@
+export class BadRequestError extends Error {
+    constructor(message: string) {
+      super(message)
+    }
+  }
+  
+  export class UserNotAuthenticatedError extends Error {
+    constructor(message: string) {
+      super(message)
+    }
+  }
+  
+  export class UserForbiddenError extends Error {
+    constructor(message: string) {
+      super(message)
+    }
+  }
+  
+  export class NotFoundError extends Error {
+    constructor(message: string) {
+      super(message)
+    }
+  }
+  
