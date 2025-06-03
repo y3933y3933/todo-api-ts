3c39c3187c9ba8407485060320600690aceb9751
Joanne Lin <y3933y3933@gmail.com>
2025-06-04 02:01:51 +0800

    feat: add refreshToken schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,17 +1,32 @@
 import { pgTable, timestamp, varchar, uuid } from "drizzle-orm/pg-core"
 
-
 export const users = pgTable("users", {
-    id: uuid("id").primaryKey().defaultRandom(),
-    createdAt: timestamp("created_at").notNull().defaultNow(),
-    updatedAt: timestamp("updated_at")
-      .notNull()
-      .defaultNow()
-      .$onUpdate(() => new Date()),
-    email: varchar("email", { length: 256 }).unique().notNull(),
-    hashedPassword: varchar("hashed_password", { length: 256 })
-      .notNull()
-      .default("unset"),
-  })
-  
-  export type NewUser = typeof users.$inferInsert
+  id: uuid("id").primaryKey().defaultRandom(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at")
+    .notNull()
+    .defaultNow()
+    .$onUpdate(() => new Date()),
+  email: varchar("email", { length: 256 }).unique().notNull(),
+  hashedPassword: varchar("hashed_password", { length: 256 })
+    .notNull()
+    .default("unset"),
+})
+
+export type NewUser = typeof users.$inferInsert
+
+export const refreshTokens = pgTable("refresh_tokens", {
+  token: varchar("token", { length: 256 }),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at")
+    .notNull()
+    .defaultNow()
+    .$onUpdate(() => new Date()),
+  userId: uuid("user_id")
+    .references(() => users.id, { onDelete: "cascade" })
+    .notNull(),
+  expiresAt: timestamp("expires_at").notNull(),
+  revokedAt: timestamp("revoked_at"),
+})
+
+export type NewToken = typeof refreshTokens.$inferInsert
