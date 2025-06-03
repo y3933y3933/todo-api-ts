96ad00f6656eae4721e4bf156a485b9113126318
Joanne Lin <y3933y3933@gmail.com>
2025-06-04 01:58:19 +0800

    feat: add jwt helper function

--- src/auth.ts
+++ src/auth.ts
@@ -1,4 +1,7 @@
 import bcrypt from "bcrypt"
+import jwt, { JwtPayload } from "jsonwebtoken"
+import { UserNotAuthenticatedError } from "./api/errors.js"
+import { config } from "./config.js"
 
 export async function hashPassword(password: string) {
   const saltRounds = 10
@@ -7,4 +10,47 @@ export async function hashPassword(password: string) {
 
 export async function checkPasswordHash(password: string, hash: string) {
   return bcrypt.compare(password, hash)
+}
+
+type payload = Pick<jwt.JwtPayload, "iss" | "sub" | "iat" | "exp">
+
+export function makeJWT(
+  userID: string,
+  expiresIn: number,
+  secret: string
+): string {
+  const issuedAt = Math.floor(Date.now() / 1000)
+  const expiresAt = issuedAt + expiresIn
+
+  const token = jwt.sign(
+    {
+      iss: config.jwt.issuer,
+      sub: userID,
+      iat: issuedAt,
+      exp: expiresAt,
+    } satisfies payload,
+    secret,
+    { algorithm: "HS256" }
+  )
+
+  return token
+}
+
+export function validateJWT(tokenString: string, secret: string): string {
+  let decoded: payload
+  try {
+    decoded = jwt.verify(tokenString, secret) as JwtPayload
+  } catch (e) {
+    throw new UserNotAuthenticatedError("Invalid token")
+  }
+
+  if (decoded.iss !== config.jwt.issuer) {
+    throw new UserNotAuthenticatedError("Invalid issuer")
+  }
+
+  if (!decoded.sub) {
+    throw new UserNotAuthenticatedError("No user ID in token")
+  }
+
+  return decoded.sub
 }
